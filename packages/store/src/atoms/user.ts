92d287a9500337935a7943fc8481bb2cff5bf7a4
asutosh-147 <behuriaasutosh7@gmail.com>
2024-05-24 23:06:57 +0530

    ws script updated

--- packages/store/src/atoms/user.ts
+++ packages/store/src/atoms/user.ts
@@ -1,5 +1,5 @@
 import { atom, selector } from "recoil";
-const BACKEND_URL = "http://localhost:3000";
+const BACKEND_URL = "https://chess-r38u.onrender.com";
 
 export type User = {
   token: string;
